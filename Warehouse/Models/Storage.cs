133169aa452ed2e52e465054f3ea5614b065a68e
Vitali Satsura <vitali.satsura@gmail.com>
2020-03-04 20:35:50 +0300

    feat: add Storage model

---
+++ Warehouse/Models/Storage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Models
+{
+    public class Storage
+    {
+        [Key]
+        public int Id { get; set; }      
+        public DateTime ReceiptDate { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string DeliveryMethod { get; set; }
+        public string Volume { get; set; }
+        public int Price { get; set; }
+    }
+}
