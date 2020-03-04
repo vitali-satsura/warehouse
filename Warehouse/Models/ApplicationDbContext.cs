e3586ad88457a2c45a6b8fa123eb36948403affd
Vitali Satsura <vitali.satsura@gmail.com>
2020-03-04 21:21:11 +0300

    feat: add ApplicationDbContext

---
+++ Warehouse/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<TypeOfProduct> TypeOfProducts { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+    }
+}
